73a4e0d738ce04999cf2ade915e8c7f603410971
Parxya <parixyanasiri@gmail.com>
2023-11-14 22:02:43 +0330

    Upload Contact-manager-app project on Github

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+const Spinner = () => {
+  return (
+    <>
+      <img
+        src={require(".././assets/Spinner.gif")}
+        className="d-block m-auto py-5"
+        style={{ width: "300px" }}
+      />
+    </>
+  );
+};
+
+export default Spinner;
